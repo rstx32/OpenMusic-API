39847b885c7d8b22fb3c8871f442270782376d0a
restu indera <rstx32@gmail.com>
2023-01-09 22:02:40 +0700

    implement error handling

--- src/api/albums/handler.js
+++ src/api/albums/handler.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-console */
 /* eslint-disable no-underscore-dangle */
 const abind = require('abind')
-const ClientError = require('../../exceptions/ClientError')
 
 class AlbumsHandler {
   constructor(service, validator) {
@@ -12,39 +11,17 @@ class AlbumsHandler {
 
   // POST Album
   async postAlbumHandler(request, h) {
-    try {
-      this._validator.validateAlbumPayload(request.payload)
-      const { name, year } = request.payload
-      const albumId = await this._service.addAlbum({ name, year })
-      const response = h
-        .response({
-          status: 'success',
-          data: { albumId },
-        })
-        .code(201)
+    this._validator.validateAlbumPayload(request.payload)
+    const { name, year } = request.payload
+    const albumId = await this._service.addAlbum({ name, year })
+    const response = h
+      .response({
+        status: 'success',
+        data: { albumId },
+      })
+      .code(201)
 
-      return response
-    } catch (error) {
-      if (error instanceof ClientError) {
-        const response = h
-          .response({
-            status: 'fail',
-            message: error.message,
-          })
-          .code(error.statusCode)
-
-        return response
-      }
-
-      const response = h
-        .response({
-          status: 'error',
-          message: 'terjadi kegagalan pada server',
-        })
-        .code(500)
-      console.error(error)
-      return response
-    }
+    return response
   }
 }
 
--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const Hapi = require('@hapi/hapi')
 const albums = require('./api/albums')
 const AlbumsService = require('./services/AlbumsService')
 const AlbumValidator = require('./validator/albums')
+const ClientError = require('./exceptions/ClientError')
 
 config({ path: '.env' })
 ;(async () => {
@@ -28,6 +29,36 @@ config({ path: '.env' })
       service: albumsService,
       validator: AlbumValidator,
     },
+  })
+
+  // error handling
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request
+    if (response instanceof Error) {
+      // penanganan client error secara internal.
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message,
+        })
+        newResponse.code(response.statusCode)
+        return newResponse
+      }
+      // mempertahankan penanganan client error oleh hapi secara native, seperti 404, etc.
+      if (!response.isServer) {
+        return h.continue
+      }
+      // penanganan server error sesuai kebutuhan
+      const newResponse = h.response({
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami',
+      })
+      newResponse.code(500)
+      return newResponse
+    }
+    // jika bukan error, lanjutkan dengan response sebelumnya (tanpa terintervensi)
+    return h.continue
   })
 
   await server.start()
