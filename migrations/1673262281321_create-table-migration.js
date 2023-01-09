28a556f22aa4c1a8142444f1025a47092edd60d7
restu indera <rstx32@gmail.com>
2023-01-09 19:35:27 +0700

    add migrations

---
+++ migrations/1673262281321_create-table-migration.js
@@ -0,0 +1,51 @@
+exports.up = (pgm) => {
+  pgm.createTable('albums', {
+    id: {
+      type: 'varchar(22)',
+      primaryKey: true,
+    },
+    name: {
+      type: 'varchar(50)',
+      notNull: true,
+    },
+    year: {
+      type: 'varchar(4)',
+      notNull: true,
+    },
+  })
+  pgm.createTable('songs', {
+    id: {
+      type: 'varchar(21)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'varchar(50)',
+      notNull: true,
+    },
+    year: {
+      type: 'varchar(4)',
+      notNull: true,
+    },
+    performer: {
+      type: 'varchar(50)',
+      notNull: true,
+    },
+    genre: {
+      type: 'varchar(50)',
+      notNull: true,
+    },
+    duration: {
+      type: 'integer',
+      notNull: true,
+    },
+    albumId: {
+      type: 'varchar(22)',
+      notNull: true,
+    },
+  })
+}
+
+exports.down = (pgm) => {
+  pgm.dropTable('albums')
+  pgm.dropTable('songs')
+}
