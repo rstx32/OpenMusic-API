bb901120b2f32d23af70e83b23626e4271acb203
restu indera <rstx32@gmail.com>
2023-01-10 15:16:54 +0700

    add song api

--- src/server.js
+++ src/server.js
@@ -5,13 +5,17 @@
 const { config } = require('dotenv')
 const Hapi = require('@hapi/hapi')
 const albums = require('./api/albums')
+const songs = require('./api/songs')
 const AlbumsService = require('./services/AlbumsService')
+const SongsService = require('./services/SongsService')
 const AlbumValidator = require('./validator/albums')
+const SongValidator = require('./validator/songs')
 const ClientError = require('./exceptions/ClientError')
 
 config({ path: '.env' })
 ;(async () => {
   const albumsService = new AlbumsService()
+  const songsService = new SongsService()
 
   const server = new Hapi.server({
     host: process.env.HOST,
@@ -28,6 +32,13 @@ config({ path: '.env' })
     options: {
       service: albumsService,
       validator: AlbumValidator,
+    },
+  })
+  await server.register({
+    plugin: songs,
+    options: {
+      service: songsService,
+      validator: SongValidator,
     },
   })
 
---
+++ src/validator/songs/index.js
@@ -0,0 +1,14 @@
+const SongsPayloadSchema = require('./schema')
+const InvariantError = require('../../exceptions/InvariantError')
+
+const SongsValidator = {
+  validateSongPayload: (payload) => {
+    const validationResult = SongsPayloadSchema.validate(payload)
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message)
+    }
+  },
+}
+
+module.exports = SongsValidator
